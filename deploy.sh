7556bf391468a00c3ff6fcdf4a2fe29db7cd2bce
nayfus1991 <58098687+nayfus1991@users.noreply.github.com>
2024-04-24 14:24:52 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 docker login -u nayfus1991 -p dckr_pat_jx46ts4rOgQ_RgsqQ1TM42odw24
-docker tag capstone nayfus1991/dev
-docker push nayfus1991/dev
+docker tag capstone nayfus1991/prod
+docker push nayfus1991/prod
 
